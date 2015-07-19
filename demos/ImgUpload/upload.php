553bc027469ffb29d2225c9955ad9711bbc64977
lumixraku <lumixraku@gmail.com>
2015-07-19 18:58:26 +0800

    img upload demo

--- demos/ImgUpload/upload.php
+++ demos/ImgUpload/upload.php
@@ -1,22 +1,32 @@
 <?php
 
 $uploaddir = 'img/';
-$src_array=array();
+// $src_array=array();
+// if(isset($_FILES['files'])){
+//     foreach ($_FILES['files']["error"] as $key => $error) {
+//         if($error==UPLOAD_ERR_OK){
+//             header("Content-Length: ".$_FILES["files"]["size"][$key]);
+//             if(!preg_match("#image#",$_FILES["files"]["type"][$key]))continue;
+//             $tmp_name = $_FILES["files"]["tmp_name"][$key];
+//             $name = $_FILES["files"]["name"][$key];
+//             $name= date("YmdHis",time()).preg_replace("#[^\w\.]#","",$name);
+//             $uploadfile = $uploaddir.$name;
+//             echo $uploadfile;
+//             $ret=move_uploaded_file($tmp_name, $uploadfile);
+//             if($ret){
+//                 $src_array[]=$uploadfile;
+//             }
+//         }
+//     }
+// }
 
-if(isset($_FILES['files'])){
-    foreach ($_FILES['files']["error"] as $key => $error) {
-        if($error==UPLOAD_ERR_OK){
-            if(!preg_match("#image#",$_FILES["files"]["type"][$key]))continue;
-            if($_FILES["files"]["size"][$key]>1024*400)continue;
-            $tmp_name = $_FILES["files"]["tmp_name"][$key];
-            $name = $_FILES["files"]["name"][$key];
-            $name= date("YmdHis",time()).preg_replace("#[^\w\.]#","",$name);
-            $uploadfile = $uploaddir.$name;
-            $ret=move_uploaded_file($tmp_name, $uploadfile);
-            if($ret){
-                $src_array[]=$uploadfile;
-            }
-        }
-    }
-}
-echo json_encode($src_array);
+header("Content-Length: ".$_FILES["file"]["size"]);
+$tmp_name = $_FILES["file"]["tmp_name"];
+$name = $_FILES["file"]["name"];
+$name= date("YmdHis",time()).preg_replace("#[^\w\.]#","",$name);
+$uploadfile = $uploaddir.$name;
+$ret=move_uploaded_file($tmp_name, $uploadfile);
+echo $tmp_name.'    ';
+echo $uploadfile;
+echo $_FILES["file"]["size"];
+?>
