148da308f1b0a13598f1e566d0949de637889a50
lumixraku <lumixraku@gmail.com>
2015-10-02 17:53:00 +0800

    update 2048

--- demos/2048/2048.js
+++ demos/2048/2048.js
@@ -36,21 +36,21 @@ function init() {
 }
 
 function bindEvents() {
-    var btns = document.querySelector('.btns');
-    btns.addEventListener('click', function(e) {
-        var target = e.target;
-        if (target.classList.contains('btnUp')) {
-            swipeUp();
-        } else if (target.classList.contains('btnDown')) {
-            swipeDown();
-        } else if (target.classList.contains('btnLeft')) {
-            swipeLeft();
-        } else if (target.classList.contains('btnRight')) {
-            swipeRight();
-        }
-    }, false);
-    window.addEventListener('keydown', function(e){
-        switch(e.keyCode){
+    // var btns = document.querySelector('.btns');
+    // btns.addEventListener('click', function(e) {
+    //     var target = e.target;
+    //     if (target.classList.contains('btnUp')) {
+    //         swipeUp();
+    //     } else if (target.classList.contains('btnDown')) {
+    //         swipeDown();
+    //     } else if (target.classList.contains('btnLeft')) {
+    //         swipeLeft();
+    //     } else if (target.classList.contains('btnRight')) {
+    //         swipeRight();
+    //     }
+    // }, false);
+    window.addEventListener('keydown', function(e) {
+        switch (e.keyCode) {
             case 87:
                 swipeUp();
                 break;
@@ -64,6 +64,42 @@ function bindEvents() {
                 swipeRight();
                 break;
         }
+    }, false);
+    var touchStart = {
+            x: 0,
+            y: 0,
+        },
+        touchEnd = {
+            x: 0,
+            y: 0
+        }
+    window.addEventListener('touchstart', function(e) {
+        touchStart.x = e.touches[0].pageX;
+        touchStart.y = e.touches[0].pageY;
+        e.preventDefault();
+    }, false);
+    window.addEventListener('touchmove', function(e) {
+        touchEnd.x = e.touches[0].pageX;
+        touchEnd.y = e.touches[0].pageY;
+        e.preventDefault();
+    }, false);
+    window.addEventListener('touchend', function(e) {
+        //UP DWON
+        if (Math.abs(touchEnd.y - touchStart.y) > Math.abs(touchEnd.x - touchStart.x)) {
+            if (touchEnd.y < touchStart.y) {
+                swipeUp();
+            } else {
+                swipeDown();
+            }
+        }
+        if (Math.abs(touchEnd.x - touchStart.x) > Math.abs(touchEnd.y - touchStart.y)) {
+            if (touchEnd.x < touchStart.x) {
+                swipeLeft();
+            } else {
+                swipeRight();
+            }
+        }
+        e.preventDefault();
     }, false);
 }
 
@@ -80,10 +116,10 @@ function addBlock(x, y, value) {
     var pos = getPosition(x, y);
     var block = newBlockELe();
     if (value) {
-        block.setAttribute('data',value);
+        block.setAttribute('data', value);
         block.innerText = value + '';
     } else {
-        block.setAttribute('data',0);
+        block.setAttribute('data', 0);
         block.classList.add('dark');
     }
     block.style.top = pos.y + 'rem';
@@ -99,11 +135,11 @@ function getBlockBy(x, y) {
 function setBlock(x, y, value) {
     var block = blockList[x - 1][y - 1];
     if (value) {
-        block.setAttribute('data',value);
+        block.setAttribute('data', value);
         block.innerText = value;
         block.classList.remove('dark');
     } else {
-        block.setAttribute('data',0);
+        block.setAttribute('data', 0);
         block.innerText = '';
         block.classList.add('dark');
     }
@@ -128,8 +164,8 @@ function blockMerge(dist, curr) {
             // });
         } else if (dist.getAttribute('data') == curr.getAttribute('data')) {
             toDark(curr);
-            moved  = true;
-            dist.setAttribute('data', (+dist.getAttribute('data'))* 2);
+            moved = true;
+            dist.setAttribute('data', (+dist.getAttribute('data')) * 2);
             // animate(dist, curr, value).then(function(){
             //     if(dist.getAttribute('data') != '0'){
             //         dist.innerText = dist.getAttribute('data');
@@ -151,7 +187,7 @@ function toDark(ele) {
     ele.classList.add('dark');
 }
 
-function animate(dist, curr, value){
+function animate(dist, curr, value) {
     var distLeft = +parseFloat(dist.style.left).toFixed(1),
         distTop = +parseFloat(dist.style.top).toFixed(1),
         currentTop = +parseFloat(curr.style.top).toFixed(1),
@@ -165,62 +201,58 @@ function animate(dist, curr, value){
     animate.style.left = currentLeft + 'rem';
     animate.innerText = value;
     wrapper.appendChild(animate);
-    return new Promise(function(resolve, reject){
-        function refreshDist(){
-            if(dist.getAttribute('data') != '0'){
+    return new Promise(function(resolve, reject) {
+        function refreshDist() {
+            if (dist.getAttribute('data') != '0') {
                 dist.innerText = dist.getAttribute('data');
                 dist.classList.remove('dark');
-            }else{
+            } else {
                 dist.classList.add('dark');
                 dist.innerText = '';
             }
         }
-        var timer = setInterval(function(){
-            if(distTop < currentTop){
-                curTop = curTop -0.5;
-                animate.style.top = curTop +'rem';
-                if(curTop <= distTop){
+        var timer = setInterval(function() {
+            if (distTop < currentTop) {
+                curTop = curTop - 0.2;
+                animate.style.top = curTop + 'rem';
+                if (curTop <= distTop) {
                     clearInterval(timer);
-                    animate.style.top = distTop + 'rem';
                     animate.remove();
                     refreshDist();
                     resolve();
                 }
             }
-            if(distTop > currentTop){
-                curTop = curTop + 0.5;
-                animate.style.top = curTop +'rem';
-                if(curTop >= distTop){
+            if (distTop > currentTop) {
+                curTop = curTop + 0.2;
+                animate.style.top = curTop + 'rem';
+                if (curTop >= distTop) {
                     clearInterval(timer);
-                    animate.style.top = distTop + 'rem';
                     animate.remove();
                     refreshDist();
                     resolve();
                 }
             }
-            if(distLeft < currentLeft ){
-                curLeft = curLeft - 0.5;
+            if (distLeft < currentLeft) { // 左移
+                curLeft = curLeft - 0.2;
                 animate.style.left = curLeft + 'rem';
-                if(curLeft <= distLeft){
+                if (curLeft <= distLeft) {
                     clearInterval(timer);
-                    animate.style.left = distLeft + 'rem';
                     animate.remove();
                     refreshDist();
                     resolve();
                 }
             }
-            if (distLeft > curLeft) {
-                curLeft = curLeft + 0.5;
+            if (distLeft > curLeft) { // 右移
+                curLeft = curLeft + 0.2;
                 animate.style.left = curLeft + 'rem';
-                if(curLeft >= distLeft){
+                if (curLeft >= distLeft) {
                     clearInterval(timer);
-                    animate.style.left = distLeft + 'rem';
                     animate.remove();
                     refreshDist();
                     resolve();
                 }
             }
-        },20);
+        }, 1);
     });
 }
 
@@ -243,17 +275,17 @@ function animateTransition(dist, curr, value) {
     animate.style.transitionDuration = '150ms';
     wrapper.appendChild(animate);
     //animate.addEventListener('transitionend', function(e) {
-        // e.target.remove();
-    setTimeout(function(){
+    // e.target.remove();
+    setTimeout(function() {
         animate.parentElement.removeChild(animate);
-        if(dist.getAttribute('data') != '0' ){
+        if (dist.getAttribute('data') != '0') {
             dist.innerText = dist.getAttribute('data');
-        }else{
+        } else {
             dist.innerText = '';
         }
-        if(curr.getAttribute('data') != '0' ){
+        if (curr.getAttribute('data') != '0') {
             curr.innerText = curr.getAttribute('data');
-        }else{
+        } else {
             curr.innerText = '';
         }
     }, 150);
@@ -276,17 +308,20 @@ function swipeUp() {
     //对于后面三排方块 每一个都检测能否上移
     for (var x = 1; x <= 4; x++) {
         for (var y = 2; y <= 4; y++) { //后三排
-            var currentY = y, distY = y,step =0, moved, value;
+            var currentY = y,
+                distY = y,
+                step = 0,
+                moved, value;
             value = getBlockBy(x, y).getAttribute('data');
             while (currentY != 1) {
                 moved = blockMerge(getBlockBy(x, currentY - 1), getBlockBy(x, currentY));
                 currentY--;
-                if(moved) {
+                if (moved) {
                     distY = currentY;
                     // step++;
                 }
             }
-            if(distY != y){ //表示该方块移动过 开始动画
+            if (distY != y) { //表示该方块移动过 开始动画
                 animate(getBlockBy(x, distY), getBlockBy(x, y), value);
             }
         }
@@ -297,16 +332,19 @@ function swipeUp() {
 function swipeDown() {
     for (var x = 1; x <= 4; x++) {
         for (var y = 3; y >= 1; y--) {
-            var currentY = y, distY = y,step =0, moved, value;
+            var currentY = y,
+                distY = y,
+                step = 0,
+                moved, value;
             value = getBlockBy(x, y).getAttribute('data');
             while (currentY != 4) {
                 moved = blockMerge(getBlockBy(x, currentY + 1), getBlockBy(x, currentY));
                 currentY++;
-                if(moved) {
+                if (moved) {
                     distY = currentY;
                 }
             }
-            if(distY != y){ //表示该方块移动过 开始动画
+            if (distY != y) { //表示该方块移动过 开始动画
                 animate(getBlockBy(x, distY), getBlockBy(x, y), value);
             }
         }
@@ -317,16 +355,19 @@ function swipeDown() {
 function swipeLeft() {
     for (var y = 1; y <= 4; y++) {
         for (var x = 2; x <= 4; x++) {
-            var currentX = x, distX = x,step =0, moved, value;
+            var currentX = x,
+                distX = x,
+                step = 0,
+                moved, value;
             value = getBlockBy(x, y).getAttribute('data');
             while (currentX != 1) {
                 moved = blockMerge(getBlockBy(currentX - 1, y), getBlockBy(currentX, y));
                 currentX--;
-                if(moved) {
+                if (moved) {
                     distX = currentX;
                 }
             }
-            if(distX != x){ //表示该方块移动过 开始动画
+            if (distX != x) { //表示该方块移动过 开始动画
                 animate(getBlockBy(distX, y), getBlockBy(x, y), value);
             }
         }
@@ -337,16 +378,19 @@ function swipeLeft() {
 function swipeRight() {
     for (var y = 1; y <= 4; y++) {
         for (var x = 3; x >= 1; x--) {
-            var currentX = x, distX = x,step =0, moved, value;
+            var currentX = x,
+                distX = x,
+                step = 0,
+                moved, value;
             value = getBlockBy(x, y).getAttribute('data');
             while (currentX != 4) {
                 moved = blockMerge(getBlockBy(currentX + 1, y), getBlockBy(currentX, y));
                 currentX++;
-                if(moved) {
+                if (moved) {
                     distX = currentX;
                 }
             }
-            if(distX != x){ //表示该方块移动过 开始动画
+            if (distX != x) { //表示该方块移动过 开始动画
                 animate(getBlockBy(distX, y), getBlockBy(x, y), value);
             }
         }
@@ -355,27 +399,74 @@ function swipeRight() {
 }
 
 function randomAdd(type) {
-    var block, randomArr = [1, 2, 3, 4], value;
+    var block, randomArr = [1, 2, 3, 4],
+        value, emptyList = [];
     if (type == UP) {
-        block = getBlockBy(Math.ceil(Math.random() * 4), 4);
+        emptyList = findEmpty(0, 4);
     } else if (type == DOWN) {
-        block = getBlockBy(Math.ceil(Math.random() * 4), 1);
+        emptyList = findEmpty(0, 1);
     } else if (type == LEFT) {
-        block = getBlockBy(4, Math.ceil(Math.random() * 4));
+        emptyList = findEmpty(4, 0);
     } else if (type == RIGHT) {
-        block = getBlockBy(1, Math.ceil(Math.random() * 4));
+        emptyList = findEmpty(1, 0);
     }
-    block.style.transform = 'scale(0.1)';
+    if (!emptyList.length) {
+        return;
+    }
+    // 如果length是4  产生0-3的随机数字
+    var rindex = ~~(Math.random() * (emptyList.length));
+    block = emptyList[rindex];
+    if (!block) {
+        debugger
+        console.log(rindex);
+    }
     value = Math.random() > 0.5 ? 4 : 2;
     block.setAttribute('data', value);
-    block.innerText = value;
-    block.classList.remove('dark');
-    block.style.opacity = '0.5';
-    setTimeout(function() {
-        block.style.transitionProperty = 'all';
-        block.style.transitionDuration = '1s';
-        block.style.transform = 'scale(1.0)';
-        block.style.opacity = '1';
-    }, 0);
+    var animateEle = document.createElement('div');
+    animateEle.style.width = BLOCK_SIZE + 'rem';
+    animateEle.style.height = BLOCK_SIZE + 'rem';
+    animateEle.style.position = 'absolute';
+    animateEle.style.top = block.style.top;
+    animateEle.style.left = block.style.left;
+    animateEle.classList.add('block');
+    animateEle.innerText = block.getAttribute('data');
+    var scale = 0.1;
+    animateEle.style.transform = 'scale(' + scale + ')';
+    wrapper.appendChild(animateEle);
+    function step(){
+        scale = scale + 0.03;
+        animateEle.style.transform = 'scale(' + scale + ')';
+        if (scale >= 1) {
+            block.classList.remove('dark');
+            block.innerText = value;
+            animateEle.remove();
+            return;
+        }
+        requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
 
+    //只有在空屋上增加新方块
+    function findEmpty(x, y) {
+        var emptyList = [],
+            block;
+        if (y) {
+            var xIndex = 1;
+            for (; xIndex <= 4; xIndex++) {
+                block = getBlockBy(xIndex, y);
+                if (block.getAttribute('data')) {
+                    emptyList.push(block);
+                }
+            }
+        } else if (x) {
+            var yIndex = 1;
+            for (; yIndex <= 4; yIndex++) {
+                block = getBlockBy(x, yIndex);
+                if (block.getAttribute('data')) {
+                    emptyList.push(block);
+                }
+            }
+        }
+        return emptyList;
+    }
 }
